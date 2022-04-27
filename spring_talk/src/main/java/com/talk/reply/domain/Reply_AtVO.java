90e904e0f53afa3cb43b2e692b1fc0d3e212073a
Kimhwawon <ghktlrekd@naver.com>
2022-04-27 15:27:43 +0900

    김화원 : reply

---
+++ spring_talk/src/main/java/com/talk/reply/domain/Reply_AtVO.java
@@ -0,0 +1,9 @@
+package com.talk.reply.domain;
+
+import lombok.Data;
+
+@Data
+public class Reply_AtVO {
+	private Long reply_num;
+	private String user_id;
+}
---
+++ spring_talk/src/main/java/com/talk/reply/service/Reply_LikeServiceImpl.java
@@ -0,0 +1,11 @@
+package com.talk.reply.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Reply_LikeServiceImpl implements Reply_LikeService{
+
+	
+	
+	
+}
