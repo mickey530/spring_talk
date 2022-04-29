b246994454daf80dbe64d11453574186a11c8c3b
Kimhwawon <ghktlrekd@naver.com>
2022-04-29 14:40:41 +0900

    .

---
+++ spring_talk/src/main/java/com/talk/user/controller/OjdbcConnectionTest.java
@@ -0,0 +1,47 @@
+package com.talk.user.controller;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class OjdbcConnectionTest {
+
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testConnection() {
+		try(Connection con = DriverManager.getConnection(
+					"jdbc:oracle:thin:@localhost:1521/XEPDB1",
+					"springprj",
+					"springprj"
+				)){
+			
+			PreparedStatement prps= con.prepareStatement("select * from user_info");
+
+            ResultSet sr =prps.executeQuery();
+
+            while(sr.next()) {
+
+                log.info(sr.getObject(1));
+
+            }
+			log.info(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}	
+}
