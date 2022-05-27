3c71ed5a0cf05c54967b64ddd0f1edb39832b3d4
mickey530 <mickey530@naver.com>
2022-05-27 14:25:18 +0900

    no message

--- spring_talk/src/main/java/com/talk/gall/service/GallLikeService.java
+++
@@ -1,9 +0,0 @@
-package com.talk.gall.service;
-
-import com.talk.gall.domain.GallLikeVO;
-
-public interface GallLikeService {	
-	public void like(GallLikeVO vo);
-	public String islike(GallLikeVO vo);
-	public long likeCount(long board_num);
-}
